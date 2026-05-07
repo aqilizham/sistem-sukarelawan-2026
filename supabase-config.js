d881daf9d8e0b177f593ed75ec1960f43b2285b6
aqilizham <aqilizham2808@gmail.com>
2026-05-07 16:47:10 +0800

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUKARELAWAN_SUPABASE_CONFIG = {
-  url: "https://pqouqistcackcpsdmfnx.supabase.co",
+  url: "https://pqouqistcakckpsdmfnx.supabase.co",
   anonKey: "sb_publishable_9KaydEsz06OquF6iuZlWZw_75FV63bs"
 };
 
